f8844cdb0d62fb5042b6554f3caadf1e531ddc89
Mariano Hilario <marianohilario@gmail.com>
2023-05-01 17:49:20 -0300

    Estrategias de Autenticación

---
+++ 9 - Estrategias de autenticación/files/src/routes/login.js
@@ -0,0 +1,81 @@
+import { Router } from "express"
+import { userVali } from "../middleware/userValidation.js"
+import { MongoUserManager } from "../dao/mongo/mongoUserManager.js"
+import { createHash, isValidPassword } from "../ultis/bcryptPass.js"
+import passport from "passport"
+
+const router = Router()
+
+const mongoUserManager = new MongoUserManager
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/login', passport.authenticate('login', {failureRedirect: '/auth/faillogin'}), async (req, res) => {
+    if(!req.user) return res.status(400).send({status: 'error', error: 'Credenciales inválidas'})
+    const { username, password } = req.body
+    try {
+        
+        if (req.user.email === 'adminCoder@coder.com' && password === 'adminCod3r123') {
+            req.session.user = req.user.nombre
+            req.session.admin = true
+            req.session.usuario = false
+            console.log('usted es admin')
+            res.redirect('http://localhost:8080/products')
+        } else {
+            req.session.user = req.user.nombre
+            req.session.admin = false
+            req.session.usuario = true
+            console.log('usted es usuario')
+             res.redirect('http://localhost:8080/products')
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/faillogin', (req, res) => {
+    res.send({status: 'error', message: 'Falló el login'})
+})
+
+
+router.post('/register', userVali, passport.authenticate('register', {failureRedirect: '/auth/failregister'}), async (req, res)=>{
+    
+    try {
+        res.redirect('http://localhost:8080/auth/login')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/failregister', (req, res) => {
+    res.send({status: 'error', message: 'Falló el registro'})
+})
+
+router.post('/logout', async (req, res) => {
+    try {
+        req.session.destroy(err => {
+            if(!err) res.redirect('http://localhost:8080/auth/login')
+            else res.send({status:'Logout error', message: err})
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/github', passport.authenticate('github', {scope: ['user:email']}), async (req, res) => {})
+
+router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/auth/login'}), async (req, res) => {
+    req.session.user = req.user.nombre
+    req.session.admin = false
+    req.session.usuario = true
+    res.redirect('http://localhost:8080/products')
+})
+
+export default router
