c80951bdc4d38bd2384eb62453b63c78a796a2cc
Mariano Hilario <marianohilario@gmail.com>
2023-07-05 23:45:21 -0300

    Tercer práctica integradora

---
+++ 15 - Tercer práctica integradora/files/src/services/ticketService.js
@@ -0,0 +1,17 @@
+import { TicketManager } from '../dao/mongo/mongoTicketManager.js'
+import logger from "../ultis/logger.js"
+
+
+const ticketManager = new TicketManager
+
+class TicketService {
+    async createTicket(code, purchase_datetime, amount, purchaser) {
+        try {
+            return await ticketManager.createTicket(code, purchase_datetime, amount, purchaser)
+        } catch (error) {
+            logger.error(error);
+        }
+    }
+}
+
+export default TicketService
